8d255a53ee5f16a6148ed8122a16af92a0e2716f
Erez Gilat <erezgilat@gmail.com>
2023-01-01 17:01:02 +0200

    //

---
+++ ServerClass.h
@@ -0,0 +1,20 @@
+# include <iostream>
+class ServerClass
+{
+private:
+    int serverPort = 0;
+public:
+    string data;
+    ServerClass(/* args */);
+    ~ServerClass();
+};
+
+ServerClass::ServerClass(/* args */)
+{
+
+}
+
+ServerClass::~ServerClass()
+{
+
+}
